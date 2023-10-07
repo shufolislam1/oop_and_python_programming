382ab6f1476897ac8bedeb9bc4358d5f9d0e8b9c
shufol islam <shufolislam1255@gmail.com>
2023-10-07 15:20:53 +0600

    w2pd1 starteed

---
+++ w2pd1/simple_shop.py
@@ -0,0 +1,14 @@
+class Product:
+    def __init__(self) -> None:
+        pass
+
+
+class Shop:
+
+    def add_product(self):
+        pass
+        # this method add products using product class to the shop class
+
+    def buy_product(self):
+        pass
+        # used to buy a product and check whether this product is available or not. If you successfully buy a product, then throw a Congress message.
