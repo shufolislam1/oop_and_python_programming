8583bbb4d2a8b8abfebe672652defceea27ee803
shufol islam <shufolislam1255@gmail.com>
2023-10-08 00:00:37 +0600

    module 7 staarted

--- Basic_python/auto_type.py
+++ Basic_python/auto_type.py
@@ -3,5 +3,5 @@ from time import sleep
 
 sleep(3)
 for j in range(0,3):
-    pyautogui.write('Hello world!', interval=0.25)
+    pyautogui.write('fuck you!', interval=0.25)
     pyautogui.press('enter')
