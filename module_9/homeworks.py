6b8fa034355d87b5e54d82bf5e713fc28cc03bea
shufolislam1 <shufolislam1255@gmail.com>
2023-10-12 12:11:23 +0600

    idea shared what to do in module 9. some hw

---
+++ module_9/homeworks.py
@@ -0,0 +1,13 @@
+#have to do below example using oop and start thinking how these stuffs working with opp. Also consider main pillars in these(inheritance, encaptulation, abstraction and polymorphism)
+
+"""
+1. Pharmacy Management System
+    Company, Sales Represen., Employee of pharmacy and company, medicine(types of medicine), quantity, price of medicine, total sell per day, revenue per day, gross income per day(mane sob bad 
+    dewar por total lav)
+
+2. Resturant management ssytem
+3. Yellow, Sailor, Aarong
+4.Rokomari
+5.Sonargaon University ERP.
+
+"""
